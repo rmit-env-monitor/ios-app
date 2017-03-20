612bdf450a133893a61df705b2fab53f5968ba8a
giabao0212 <ngducgiabao96@gmail.com>
2017-03-21 00:10:44 +0700

    Done resizing layout with login view

--- AirPollution/LoginController.swift
+++ AirPollution/LoginController.swift
@@ -14,18 +14,18 @@ import TextFieldEffects
 class LoginController: UIViewController {
     
     /*lazy var appNameLabel : UILabel = {
-        let lb = UILabel()
-        lb.translatesAutoresizingMaskIntoConstraints = false
-        lb.text = "AIR POLLUTION"
-        lb.textAlignment = .center
-        
-        _ = OpenSans.registerFonts()
-        lb.font = UIFont.openSansBoldFontOfSize(45)
-        lb.textColor = UIColor.white
-        lb.attributedText = NSAttributedString(string: "AIR POLLUTION", attributes: [NSStrokeColorAttributeName: UIColor.black,
-            NSStrokeWidthAttributeName: -1])
-        return lb
-    }()*/
+     let lb = UILabel()
+     lb.translatesAutoresizingMaskIntoConstraints = false
+     lb.text = "AIR POLLUTION"
+     lb.textAlignment = .center
+     
+     _ = OpenSans.registerFonts()
+     lb.font = UIFont.openSansBoldFontOfSize(45)
+     lb.textColor = UIColor.white
+     lb.attributedText = NSAttributedString(string: "AIR POLLUTION", attributes: [NSStrokeColorAttributeName: UIColor.black,
+     NSStrokeWidthAttributeName: -1])
+     return lb
+     }()*/
     
     lazy var nameTextField : YoshikoTextField = {
         let tf = YoshikoTextField()
@@ -70,16 +70,16 @@ class LoginController: UIViewController {
     }()
     
     /*lazy var registerBtn : UIButton = {
-        let btn = UIButton()
-        btn.translatesAutoresizingMaskIntoConstraints = false
-        btn.setTitle("Register", for: .normal)
-        btn.backgroundColor = UIColor.gray
-        btn.layer.cornerRadius = 20
-        btn.layer.masksToBounds = true
-        btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        btn.addTarget(self, action: #selector(handleRegister), for: .touchUpInside)
-        return btn
-    }()*/
+     let btn = UIButton()
+     btn.translatesAutoresizingMaskIntoConstraints = false
+     btn.setTitle("Register", for: .normal)
+     btn.backgroundColor = UIColor.gray
+     btn.layer.cornerRadius = 20
+     btn.layer.masksToBounds = true
+     btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+     btn.addTarget(self, action: #selector(handleRegister), for: .touchUpInside)
+     return btn
+     }()*/
     
     lazy var bgImageView : UIImageView = {
         let imageView = UIImageView()
@@ -97,14 +97,27 @@ class LoginController: UIViewController {
     var nameTextFieldTopAnchor : NSLayoutConstraint?
     var bgImageViewTopAnchor : NSLayoutConstraint?
     var loginBtnBottomAnchor : NSLayoutConstraint?
-
+    
     func dismissKeyboard() {
-        self.view.endEditing(true)
+        
+        if nameTextField.isFirstResponder {
+            UIView.animate(withDuration: 0.5, animations: { 
+                self.nameTextField.resignFirstResponder()
+            })
+        }
+        else if pwTextField.isFirstResponder {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.pwTextField.resignFirstResponder()
+            })
+        }
+        
+        
     }
     
     func setupUI() {
         //hint: add observer when keyboard appears to handle UI resizing
-        NotificationCenter.default.addObserver(self, selector: #selector(LoginController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginController.keyboardDidShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginController.keyboardDidHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
         
         //hint: add tap recognizer to dismiss keyboard
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
@@ -118,9 +131,9 @@ class LoginController: UIViewController {
         //view.addSubview(registerBtn)
         
         /*appNameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        appNameLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        appNameLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        appNameLabel.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true*/
+         appNameLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+         appNameLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+         appNameLabel.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true*/
         
         nameTextFieldTopAnchor = nameTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 350)
         nameTextFieldTopAnchor?.isActive = true
@@ -142,11 +155,12 @@ class LoginController: UIViewController {
         
         
         /*registerBtn.topAnchor.constraint(equalTo: loginBtn.bottomAnchor, constant: 20).isActive = true
-        registerBtn.leadingAnchor.constraint(equalTo: pwTextField.leadingAnchor).isActive = true
-        registerBtn.widthAnchor.constraint(equalToConstant: view.bounds.width - 100).isActive = true
-        registerBtn.heightAnchor.constraint(equalToConstant: 50).isActive = true*/
+         registerBtn.leadingAnchor.constraint(equalTo: pwTextField.leadingAnchor).isActive = true
+         registerBtn.widthAnchor.constraint(equalToConstant: view.bounds.width - 100).isActive = true
+         registerBtn.heightAnchor.constraint(equalToConstant: 50).isActive = true*/
         
-        bgImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        bgImageViewTopAnchor = bgImageView.topAnchor.constraint(equalTo: view.topAnchor)
+        bgImageViewTopAnchor?.isActive = true
         bgImageView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         bgImageView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
         bgImageView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
@@ -176,24 +190,61 @@ class LoginController: UIViewController {
         }
     }
     
-    func keyboardWillShow(notification : NSNotification) {
+    func keyboardDidShow(notification : NSNotification) {
         print("Keyboard did show")
-        nameTextFieldTopAnchor?.isActive = false
-        nameTextFieldTopAnchor = nameTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 100)
-        nameTextFieldTopAnchor?.isActive = true
+        let keyBoardHeight = getInfoOfKeyBoard(notification: notification)["height"] as? CGFloat
+        let keyBoardDuration = getInfoOfKeyBoard(notification: notification)["duration"] as? Float
+        let resizingDistance = -(keyBoardHeight!) - 10
+        self.nameTextFieldTopAnchor?.isActive = false
+        self.nameTextFieldTopAnchor = self.nameTextField.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 340 + resizingDistance)
+        self.nameTextFieldTopAnchor?.isActive = true
         
-        loginBtnBottomAnchor?.isActive = false
-        let resizingDistance = -(getHeightOfKeyBoard(notification: notification)) - 10
-        loginBtnBottomAnchor = loginBtn.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: resizingDistance)
-        loginBtnBottomAnchor?.isActive = true
+        self.loginBtnBottomAnchor?.isActive = false
+        self.loginBtnBottomAnchor = self.loginBtn.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: resizingDistance)
+        self.loginBtnBottomAnchor?.isActive = true
+        
+        self.bgImageViewTopAnchor?.isActive = false
+        self.bgImageViewTopAnchor = self.bgImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: resizingDistance + 10)
+        self.bgImageViewTopAnchor?.isActive = true
+        
+        UIView.animate(withDuration: TimeInterval(keyBoardDuration!)) {
+            self.view.layoutIfNeeded()
+        }
+        
         
     }
     
-    func getHeightOfKeyBoard(notification : NSNotification) -> CGFloat {
+    func keyboardDidHide(notification : NSNotification) {
+        print("Keyboard did Hide")
+        let keyBoardDuration = getInfoOfKeyBoard(notification: notification)["duration"] as? Float
+        
+        
+        self.nameTextFieldTopAnchor?.isActive = false
+        self.nameTextFieldTopAnchor = self.nameTextField.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 350)
+        self.nameTextFieldTopAnchor?.isActive = true
+        
+        self.loginBtnBottomAnchor?.isActive = false
+        self.loginBtnBottomAnchor = self.loginBtn.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -10)
+        self.loginBtnBottomAnchor?.isActive = true
+        self.bgImageViewTopAnchor?.isActive = false
+        self.bgImageViewTopAnchor = self.bgImageView.topAnchor.constraint(equalTo: self.view.topAnchor)
+        self.bgImageViewTopAnchor?.isActive = true
+        
+        UIView.animate(withDuration: TimeInterval(keyBoardDuration!)) {
+            self.view.layoutIfNeeded()
+            
+        }
+    }
+    
+    func getInfoOfKeyBoard(notification : NSNotification) -> [String : Any] {
         let userInfo = notification.userInfo! as Dictionary
         let keyboardFrame = userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue
         let keyboardHeight = keyboardFrame.cgRectValue.height
-        return keyboardHeight
+        let keyboardDuration = userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber
+        var keyboardInfo = [String : Any]()
+        keyboardInfo["height"] = keyboardHeight
+        keyboardInfo["duration"] = keyboardDuration
+        return keyboardInfo
     }
     
     func getToTabBarController() -> ESTabBarController {
