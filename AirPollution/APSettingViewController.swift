c6ac524daf6188b601d7c1d8a4c0047aa3579b94
giabao0212 <ngducgiabao96@gmail.com>
2017-07-23 19:36:44 +0700

    Restructure the project

---
+++ AirPollution/APSettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  APSettingViewController.swift
+//  AirPollution
+//
+//  Created by Nguyen Duc Gia Bao on 7/23/17.
+//  Copyright © 2017 Nguyen Duc Gia Bao. All rights reserved.
+//
+
+import UIKit
+
+class APSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
